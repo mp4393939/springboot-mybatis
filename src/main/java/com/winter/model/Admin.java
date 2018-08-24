71271af743f24e7bf0a22fc5cef2e32d22480adc
yangtthi <yangtthi@163.com>
2018-08-24 17:28:12 +0800

    commit

---
+++ src/main/java/com/winter/model/Admin.java
@@ -0,0 +1,43 @@
+package com.winter.model;
+
+public class Admin {
+    private Integer adminId;
+
+    private String adminName;
+
+    private String password;
+
+    private String phone;
+
+    public Integer getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Integer adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName == null ? null : adminName.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+}
